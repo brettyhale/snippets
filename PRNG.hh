b6b1025ee3064064433a9b7c70327a05dde22395
Brett Hale <brettyhale@gmail.com>
2024-01-06 20:56:52 +1000

    Add files via upload
    xoshiro256++ is enough for any purpose...

--- PRNG.hh
+++ PRNG.hh
@@ -1,8 +1,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
-// PRNG.hh : [GL0 library] PRNG (uniform random bit generator)
+// PRNG.hh : [GL0 library] (std::uniform_random_bit_generator)
 //
-// Copyright (c) 2023 Brett Hale.
+// Copyright (c) 2024 Brett Hale.
 // SPDX-License-Identifier: BSD-2-Clause
 //
 ////////////////////////////////////////////////////////////////////////////////
@@ -21,7 +21,6 @@
 #include <array>
 
 #include <random>
-#include <limits> // std::numeric_limits<>::digits
 
 
 GL0_NAMESPACE_INIT_
@@ -36,75 +35,83 @@ class PRNG
 {
 public:
 
-    // xoshiro128++ PRNG (satisfies: UniformRandomBitGenerator)
+    // xoshiro256++ PRNG (satisfies: UniformRandomBitGenerator)
 
-    // based on the 'xoshiro128plusplus.c' implementation by
+    // based on the 'xoshiro256plusplus.c' implementation by
     // David Blackman and Sebastiano Vigna: prng.di.unimi.it
 
-    using result_type = std::uint_fast32_t;
+    using result_type = std::uint_fast64_t;
 
     static constexpr result_type result_max =
-        static_cast<result_type>(UINT32_C(0xffffffff));
+        static_cast<result_type>(UINT64_C(0xffffffffffffffff));
 
     static constexpr result_type min () { return (0); }
     static constexpr result_type max () { return result_max; }
 
 
-    std::array<result_type, 4> state; // (128 bits)
+    std::array<result_type, 4> state; // (256 bits)
 
     static constexpr auto clamp (result_type u) {
         return (u & result_max); }
 
-    static constexpr auto rol (result_type u, int k) {
-        u = clamp(u); // idiomatic rotate:
-        return clamp((u << (k & 31)) | (u >> ((- k) & 31)));
+    static constexpr auto rol (result_type u, unsigned k) {
+        u = clamp(u), k %= 64; // idiomatic rotate:
+        return (k) ? clamp((u << k) | (u >> (64 - k))) : (u);
     }
 
-    // xoshiro128++ generator function:
+    // xoshiro256++ generator function:
 
     result_type operator () () noexcept {
-        auto r = rol(state[0] + state[3], (7)) + state[0];
-        auto t = clamp(state[1] << (9));
+        auto r = rol(state[0] + state[3], (23)) + state[0];
+        auto t = clamp(state[1] << (17));
         state[2] ^= state[0], state[3] ^= state[1];
         state[1] ^= state[2], state[0] ^= state[3];
-        state[2] ^= t, state[3] = rol(state[3], (11));
+        state[2] ^= t, state[3] = rol(state[3], (45));
         return clamp(r);
     }
 
-    // equivalent to (2^64) calls to generator:
+    // equivalent to (2^128) calls to generator:
 
     constexpr void jump () noexcept;
 
-    // equivalent to (2^96) calls to generator:
+    // equivalent to (2^192) calls to generator:
 
     constexpr void long_jump () noexcept;
 
 
-    // github.com/skeeto/hash-prospector : triple32
+    // Stafford's Mix13 variant:
 
     static constexpr auto mix (result_type u) {
-        u = clamp(u);
-        u = clamp((u ^ (u >> 17)) * UINT32_C(0xed5ad4bb));
-        u = clamp((u ^ (u >> 11)) * UINT32_C(0xac4c1b51));
-        u = clamp((u ^ (u >> 15)) * UINT32_C(0x31848bab));
-        return (u ^ (u >> 14));
+        u = clamp(u); u ^= (u >> 30);
+        u *= UINT64_C(0xbf58476d1ce4e5b9);
+        u = clamp(u); u ^= (u >> 27);
+        u *= UINT64_C(0x94d049bb133111eb);
+        u = clamp(u); u ^= (u >> 31);
+        return u;
     }
 
-    // (re)initialize the PRNG with a 32-bit seed:
+    // (re)initialize the PRNG with a 64 bit seed:
 
-    constexpr PRNG (result_type s = 0) noexcept
-        : state {} { seed(s); }
+    constexpr PRNG (result_type seed = 0) noexcept
+        : state {} { init(seed); }
 
-    constexpr void seed (result_type s) noexcept {
+    constexpr void init (result_type seed) noexcept {
         for (auto & si : state) // splitmix:
-            si = mix(s = clamp(s + UINT32_C(0x9e3779b9)));
+            si = mix(seed += UINT64_C(0x9e3779b97f4a7c15));
     }
 
-    // reinitialize the PRNG with a 128 bit state IV, generated
+    // (re)initialize the PRNG with a 256 bit state, generated
     // using a std::random_device:
 
-    void init (std::random_device &);
+    PRNG (std::random_device & rdev)
+        : state {} { init(rdev); }
 
+    void init (std::random_device & rdev) {
+        decltype(state) rbuf;
+        std::uniform_int_distribution<result_type> d {0, max()};
+        for (auto & ri : rbuf) ri = d(rdev);
+        state = rbuf;
+    }
 };
 
 
@@ -132,13 +139,15 @@ GL0::PRNG::jump () noexcept
     decltype(state) jbuf {};
 
     constexpr decltype(state) jtab {
-        0x8764000b, 0xf542d2d3, 0x6fa035c3, 0x77f2db5b };
+        UINT64_C(0x180ec6d33cfd0aba), UINT64_C(0xd5a61266f0c9392c),
+        UINT64_C(0xa9582618e03fc9aa), UINT64_C(0x39abdc4529b1661c)
+    };
 
     for (auto j : jtab)
     {
-        for (int b = 0; b < (32); operator () (), b++)
+        for (int b = 0; b < (64); operator () (), b++)
         {
-            if (j & (UINT32_C(1) << b))
+            if (j & (UINT64_C(1) << b))
                 for (unsigned int i = 0; i < jbuf.size(); i++)
                     jbuf[i] ^= state[i];
         }
@@ -161,53 +170,21 @@ GL0::PRNG::long_jump () noexcept
     decltype(state) jbuf {};
 
     constexpr decltype(state) jtab {
-        0xb523952e, 0x0b6f099f, 0xccf5a0ef, 0x1c580662 };
+        UINT64_C(0x76e15d3efefdcbbf), UINT64_C(0xc5004e441c522fb3),
+        UINT64_C(0x77710069854ee241), UINT64_C(0x39109bb02acbe635)
+    };
 
     for (auto j : jtab)
     {
-        for (int b = 0; b < (32); operator () (), b++)
+        for (int b = 0; b < (64); operator () (), b++)
         {
-            if (j & (UINT32_C(1) << b))
+            if (j & (UINT64_C(1) << b))
                 for (unsigned int i = 0; i < jbuf.size(); i++)
                     jbuf[i] ^= state[i];
         }
     }
 
     state = jbuf;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-//
-// GL0::PRNG init function implementations:
-
-
-inline void
-GL0::PRNG::init (std::random_device & rdev)
-{
-    decltype(state) rbuf;
-
-    if (std::numeric_limits<decltype(rdev())>::digits < (32))
-    {
-        for (auto & ri : rbuf)
-        {
-            auto lo = static_cast<result_type>(rdev());
-            auto hi = static_cast<result_type>(rdev());
-
-            ri = clamp((hi << (16)) | (lo & (0xffff)));
-        }
-    }
-    else
-    {
-        for (auto & ri : rbuf)
-            ri = clamp(static_cast<result_type>(rdev()));
-    }
-
-    // note: it shouldn't be necessary to hash values generated
-    // using a conforming std::random_device - furthermore, hash
-    // functions are not bijective, excluding possible states.
-
-    state = rbuf;
 }
 
 
